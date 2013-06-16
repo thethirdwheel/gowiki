b8f487ccad4b53613c31b72f7f9c12094c7a7503
Rory Carmichael <rcarmichael@quantcast.com>
2013-06-16 16:38:18 -0700

    transform wiki.go to a webserver

--- wiki.go
+++ wiki.go
@@ -1,33 +1,39 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
-  "net/http"
-  )
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const lenPath = len("/view/")
 
 type Page struct {
-  Title string
-  Body []byte
+	Title string
+	Body  []byte
 }
 
 func (p *Page) save() error {
-  filename := p.Title + ".txt"
-  return ioutil.WriteFile(filename, p.Body, 0600)
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
-  filename := title + ".txt"
-  body, err := ioutil.ReadFile(filename)
-  if err != nil {
-    return nil, err
-  }
-  return &Page{Title: title, Body: body}, nil
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+  title := r.URL.Path[lenPath:]
+  p, _ := loadPage(title)
+  fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
 func main() {
-  p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-  p1.save()
-  p2, _ := loadPage("TestPage")
-  fmt.Println(string(p2.Body))
+  http.HandleFunc("/view/", viewHandler)
+  http.ListenAndServe(":8080", nil)
 }
