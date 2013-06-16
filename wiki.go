41fe4b22924f7205b2db1d8d7abffdcad0f72138
Rory Carmichael <rcarmichael@quantcast.com>
2013-06-16 16:53:08 -0700

    Modify wiki.go to use templates

--- wiki.go
+++ wiki.go
@@ -28,28 +28,28 @@ func loadPage(title string) (*Page, error) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-  t, _ := template.ParseFiles(tmpl + ".html")
-  t.Execute(w, p)
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[lenPath:]
 	p, _ := loadPage(title)
-  renderTemplate(w, "view.html", p)
+	renderTemplate(w, "view.html", p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-  title := r.URL.Path[lenPath:]
-  p, err := loadPage(title)
-  if err != nil {
-    p = &Page{Title: title}
-  }
-  renderTemplate(w, "edit.html", p)
+	title := r.URL.Path[lenPath:]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit.html", p)
 }
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
-  http.HandleFunc("/edit/", editHandler)
-//  http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/edit/", editHandler)
+	//  http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
